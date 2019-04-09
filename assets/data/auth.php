1f968e1fe1a06ed0e346f48267fd6276c92b1cae
Antonio Cruces Rodríguez <35885843+antoniocruces@users.noreply.github.com>
2019-04-09 09:36:19 +0200

    Update auth.php

--- assets/data/auth.php
+++ assets/data/auth.php
@@ -13,7 +13,7 @@ if(isset($_REQUEST['q']) && isset($_REQUEST['f']) && isset($_REQUEST['c']) && is
 	$selector	= $_REQUEST['x'];
 	$credential = $_REQUEST['u']; 
 	
-	$ch  = curl_init("http://expofinder.uma.es/wp-admin/admin-ajax.php?q=$query&action=csl_generic_ajax_call&f=$filter&x=$selector&u=$credential");
+	$ch  = curl_init("http://expofinder.uma.es/wp-admin/admin-ajax.php?q=$query&action=xxx&f=$filter&x=$selector&u=$credential");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 	curl_setopt($ch, CURLOPT_USERAGENT, "PATHFINDER");
